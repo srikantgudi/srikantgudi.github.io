bf24bc5bd3752f037f3bbdcc5955ac51325e1bda
Srikant <srikantgudi@Srikants-MacBook-Pro.local>
2020-09-06 05:24:43 +0530

    Altered layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
-	let profiles = [
+let profiles = [
 		'Senior software professional with over 20 years of experience',
 	  '2005 - 2013: Web development experience using PHP, MySQL',
-	  '2014 onwards into UI development',
+	  '2014 onwards into UI development. Skills used Angular, React, Svelte, Stencil, NodeJs, MySql',
 	  'Can create wire-frames, POCs'
 	];
 	let skills = [
@@ -39,189 +39,212 @@
 				'Interacted with client-side developers to understand the requirements',
 				'Integrated APIs in the frontend code'
 			]
+		},
+		{
+			header: {
+				dates: '20-Aug-2014 to 22-Jan-2015',
+				jobTitle: 'Senior Software Developer',
+				org: 'Basil Communications Pvt Ltd, Bengaluru',
+			},
+			content: [
+				'Created HTML mockups for various applications',
+				'Interacted with designers to understand the designs',
+				'Used Photoshop to slice images'
+			]
 		}
 	];
 	let curExp = expList[0];
 </script>
 
 <style>
-	* {
-		margin: 0;
-		font-family: Helvetica;
+	:root {
+		background: #adf;
+	}
+	.info {
+		position: absolute;
+		top: 1vh;
+		left: 30vw;
+		height: 10vh;
+		width: 40vw;
+		border-radius: 20px 20px 4px 4px;
+		padding: 30px;
+		box-shadow: 0 0 4px #999999;
+		background: linear-gradient(#0ef, #dff);
+		text-align: center;
+	}
+	.info:hover {
+		z-index: 999;
+		box-shadow: 0 4px 20px #333333;
+	}
+	.info-title {
+		font-family: Georgia;
+		font-style: italic;
+		text-shadow: 1px 2px 2px navy;
+		font-size: 30px;
+		font-weight: 600;
+	}
+	.section-title {
+		background-color: #009;
+		color: #ffa;
+		padding: 10px 20px;
+		border-radius: 10px 10px 4px 4px;
+		font-size: 20px;
+		font-weight: 600;
+	}
+	.profile {
+		position: absolute;
+		top: 14vh;
+		left: 5vw;
+		height: 30vh;
+		width: 50vw;
+		padding: 20px;
+		border-radius: 20px 20px 8px 8px;
+		background-color: #def;
+		color: #666;
+		box-shadow: 0 0 4px #999999;
+	}
+	.profile .text {
+		color: #333333;
+		font-size: 20px;
+	}
+	.profile:hover {
+		z-index: 9999;
+		box-shadow: 0 4px 20px #333333;
+	}
+	.skills {
+		position: absolute;
+		top: 14vh;
+		right: 5vw;
+		min-height: 32vh;
+		width: 36vw;
+		padding: 20px;
+		border-radius: 20px 20px 8px 8px;
+		background-color: #def;
+		color: #666;
+		box-shadow: 0 0 4px #999999;
+	}
+	.skills .content {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 4px;
+	}
+	.skills:hover {
+		z-index: 9999;
+		box-shadow: 0 4px 20px #333333;
+	}
+	.exp {
+		position: absolute;
+		top: 24vh;
+		left: 10vw;
+		height: 66vh;
+		width: 80vw;
+		padding: 20px;
+		border-radius: 20px 20px 8px 8px;
+		background-color: #def;
+		color: #666;
+		box-shadow: 0 4px 20px #333333;
+	}
+	.exp-box {
+		display: grid;
+		grid-template-rows: auto 1fr;
+		grid-gap: 2px;
+	}
+	.exphdr {
+		background-color: #999999;
+		color: #f7f7f7;
+		padding: 4px 10px;
+		font-size: 14px;
+		cursor: pointer;
+	}
+	.expdetail {
+		background-color: #eeeeee;
+		color: #333333;
+		padding: 4px 10px;
+		font-size: 14px;
 	}
 	.bold {
 		font-weight: 600;
 	}
-	#my-app {
-		width: 96vw;
-		margin: 0 auto;
-		font-family: Helvetica;
-	}
-	#top-nav {
-		box-shadow: 0 0 4px #999999;
-		padding: 4px 10px;
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		grid-gap: 10px;
-		grid-template-areas: 'name contact';
-		background-color: #eeeeee;
-	}
-	.profile-title {
-		position: fixed;
-		z-index: 999;
-		top: 20px;
-		left: 30vw;
-		width: 30vw;
-		box-shadow: 0 0 4px #999999;
-		padding: 10px;
-		text-align: center;
-		font-family: 'Trebuchet MS';
-		font-size: 24px;
-		background-color: navy;
-		color: lightcyan;
-		border-radius: 10px 10px 4px 4px;
-	}
-	.box {
-		box-shadow: 0 4px 2px navy;
-		border-radius: 0 0 4px 4px;
-	}
-	#name {
-		grid-area: name;
-	}
-	#name .title {
-		font-family: cursive;
-		font-size: 30px;
-		font-weight: 600;
-		color: #00f;
-		text-shadow: 1px 2px 2px lightblue;
-	}
-	#contact {
-		grid-area: contact;
-		text-align: right;
-		line-height: 30px;
-	}
-	#contact .mail {
-		text-decoration: none;
-	}
-	.profile, .skills {
-		margin-bottom: 10px;
-		border-radius: 0 0 4px 4px;
-	}
-	.content {
-		display: grid;
-		grid-template-columns: 1fr 2fr;
-		grid-gap: 10px;
-	}
-	.left-panel {
-	}
-	.right-panel {
-	}
-	.workexp {
-		box-shadow: 0 0 4px olive;
-		margin-top: 10px;
-	}
-	.exp-hdr-title {
-		box-shadow: 0 0 4px #999999;
-		margin-bottom: 8px;
-	}
-	.org {
-		font-weight: 600;
-	}
-	.exp {
-		margin-bottom: 4px;
-		display: block;
-	}
-	.exphdr {
-		grid-area: exphdr;
-		background-color: #def;
-		padding: 4px 10px;
-		font-size: 14px;
-		font-weight: 600;
-		cursor: pointer;
-	}
-	.exphdr.activehdr {
-		box-shadow: 0 2px 8px #666;
-		background-color: #666;
-		color: lightcyan;
-	}
-	.expdetail {
-		grid-area: expdetail;
-		height: auto;
-		margin: 0 0 8px;
-		padding: 10px;
-	}
-	.activehdr, .section-title {
-		background-color: lightblue;
-		color: navy;
-		padding: 4px 10px;
-	}
-	.section-title {
-		margin-top: 4px;
-	}
-	.profile-content {
-		padding: 10px;
-	}
 </style>
 
-<main id="my-app">
-	<div id="top-nav">
-		<div id='name'>
-			<div class='title'>
-				Srikant Gudi
+<main>
+	<div class="info">
+		<div class='info-title'>SRIKANT GUDI</div>
+		<div>Bengaluru, India</div>
+		<div>srikantgudi@gmail.com | +91-892-665-6336</div>
+	</div>
+	<div class="profile">
+		<div class="section-title">
+			Profile
+		</div>
+		<ul class='text'>
+			{#each profiles as line}
+				<li>{line}</li>
+			{/each}
+		</ul>
+	</div>
+	
+	<div class="skills">
+		<div class="section-title">
+			Technical Skills
+		</div>
+		<div class="content">
+			<div>
+				<ul>
+					<li>JavaScript</li>
+					<li>HTML5</li>
+					<li>CSS3</li>
+				</ul>
+				<ul>
+					<li>Bootstrap</li>
+					<li>Angular Material</li>
+					<li>Material-UI</li>
+					<li>Vuetify</li>
+					<li>Buefy</li>
+				</ul>
 			</div>
 			<div>
-				Bengaluru, India
+				<ul class="bold">
+					<li>Angular</li>
+					<li>Svelte</li>
+					<li>Stencil</li>
+					<li>React</li>
+					<li>Vue</li>
+					<li>Nuxt</li>
+				</ul>
+				<ul>
+					<li>Git</li>
+					<li>Bitbucket</li>
+					<li>Confluence</li>
+					<li>Visual Studio Code</li>
+					<li>IntelliJ</li>
+				</ul>
 			</div>
-		</div>
-		<div id='contact'>
-			<div class="mail">srikantgudi@gmail.com</div>
-			<div>+91 829-665-6336</div>
 		</div>
 	</div>
-	<div class='content'>
-		<div class="profile-title">Senior Frontend Engineer</div>
-		<div class='left-panel'>
-			<div class='profile box'>
-				<h3 class='section-title'>Summary</h3>
-				<div class='profile-content'>
-					<ul>
-					{#each profiles as line}
-						<li>{line}</li>
-					{/each}
-					</ul>
-				</div>
-			</div>
-			<div class='skills box'>
-				<h3 class='section-title'>Skills</h3>
-				<div class='profile-content'>
-					<ul>
-						{#each skills as skill}
-							<li>{skill}</li>
-						{/each}
-					</ul>
-				</div>
-			</div>
+	
+	<div class="exp">
+		<div class="section-title">
+			Experience
 		</div>
-		<div class='right-panel'>
-			<h3 class='section-title'>Experience</h3>
-			<div class='workexp'>
-				{#each expList as exp}
-					<div class='exp' on:click={() => curExp = exp}>
-						<div class='exphdr' class:activehdr={curExp === exp}>
-							{exp.header.dates} :: {exp.header.jobTitle} - {exp.header.org}
-						</div>
-						{#if curExp === exp}
-							<div class='expdetail'>
-								<ul>
-									{#each curExp.content as line}
-										<li>{line}</li>
-									{/each}
-								</ul>
-							</div>
-						{/if}
+		<div>
+			{#each expList as exp}
+				<div class='exp-box' on:mouseover={() => curExp = exp}>
+					<div class='exphdr' class:activehdr={curExp === exp}>
+						{exp.header.dates} :: {exp.header.jobTitle} - {exp.header.org}
 					</div>
-				{/each}
-			</div>
+					{#if curExp === exp}
+						<div class='expdetail'>
+							<ul>
+								{#each curExp.content as line}
+									<li>{line}</li>
+								{/each}
+							</ul>
+						</div>
+					{/if}
+				</div>
+			{/each}
 		</div>
 	</div>
+
 </main>
