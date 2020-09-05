a20f51be31e63ed83b358ca77b7c5c5ff5a040df
Srikant <srikantgudi@Srikants-MacBook-Pro.local>
2020-09-05 22:45:28 +0530

    Added experience details

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,38 @@
 		'Angular, Svelte, Stencil, React, Vue, Nuxt',
 		'MySql, Mongodb',
 		'Git, Bitbucket, Confluence'
-	]
+	];
+	let opt = 1;
+
+	let expList = [
+		{
+			header: {
+				dates: '16-Jun-2016 to present',
+				jobTitle: 'Project Lead - System',
+				org: 'Mphasis, Bengaluru',
+			},
+			content: [
+				'Worked offshore in Bengaluru with a banking conglomerate on  projects in Angular and React',
+				'Worked onsite in Glasgow, UK with a banking conglomerate on Angular based projects',
+				'Worked offshore at client-side with Schneider Electric on Management Dashboard using Angular 1.x',
+				'Developed UI for a POC for Digital Readiness Analysis Tool to created a survey questionnaire for various categories using Angular, Material Design, HTML5, CSS3, JavaScript, API integration'
+			]
+		},
+		{
+			header: {
+				dates: '15-Aug-2015 to 14-Jan-2016',
+				jobTitle: 'Senior AngularJS Developer',
+				org: 'Pandera Solutions India Pvt Ltd, Bengaluru',
+			},
+			content: [
+				'Worked in Angular on various projects',
+				'Created sub-components for re-use',
+				'Interacted with client-side developers to understand the requirements',
+				'Integrated APIs in the frontend code'
+			]
+		}
+	];
+	let curExp = expList[0];
 </script>
 
 <main id="my-app">
@@ -30,7 +61,7 @@
 	</div>
 	<div id="profile">
 		<div class='box'>
-			<h2>Summary</h2>
+			<h3>Summary</h3>
 			<ul>
 			{#each profiles as line}
 				<li>{line}</li>
@@ -38,10 +69,44 @@
 			</ul>
 		</div>
 		<div class='box'>
-			<h2>Skills</h2>
+			<h3>Skills</h3>
 			{#each skills as skill}
 				<li>{skill}</li>
 			{/each}
+		</div>
+	</div>
+	<h3>Experience</h3>
+	<div id='workexp'>
+		<!--<div class="exp" on:click={() => opt = 2}>
+			<div class="exphdr" class:bold={opt===2}>exp-2</div>
+			<div class:expactive={opt === 2} class:exphide={opt !== 2} class="expcontent">exp-1-content</div>
+		</div>
+		<div class="exp" on:click={() => opt = 3}>
+			<div class="exphdr" class:bold={opt===3}>exp-3</div>
+			<div class:expactive={opt === 3} class:exphide={opt !== 3}  class="expcontent">exp-1-content</div>
+		</div>
+		<div class="workexp" on:click={() => opt = 4}>
+			<div class="exphdr" class:bold={opt===4}>exp-1</div>
+			<div class:expactive={opt === 4} class:exphide={opt !== 4}  class="expcontent">exp-1-content</div>
+		</div> -->
+		<div class='exp-left'>
+			{#each expList as exp}
+				<div on:click={() => curExp = exp} class='exp-hdr-title'>{exp.header.org}</div>
+			{/each}
+		</div>
+		<div class='exp-detail'>
+			<div class='exp'>
+				<div class='exphdr'>
+					{curExp.header.dates} :: {curExp.header.jobTitle} - {curExp.header.org}
+				</div>
+				<div class='expcontent'>
+					<ul>
+						{#each curExp.content as line}
+							<li>{line}</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
@@ -49,6 +114,16 @@
 	* {
 		margin: 0;
 		font-family: Helvetica;
+	}
+	.expactive {
+		display: block;
+		background-color: #def;
+	}
+	.exphide {
+		display: none;
+	}
+	.bold {
+		font-weight: 600;
 	}
 	#my-app {
 		width: 90vw;
@@ -92,11 +167,52 @@
 	#profile > div {
 		padding: 8px 20px;
 	}
-	li {
-		list-style: none;
-		line-height: 1.5;
+	#workexp {
+		box-shadow: 0 0 2px #333333;
+		display: grid;
+		grid-template-columns: 30vw 1fr;
+		height: 32vw;
 	}
-	li:before {
-		content: '> ';
+	.exp-left {
+		border-right: 1px solid #dddddd;
+		height: inherit;
+		overflow-y: auto;
+	}
+	.exp {
+		display: grid;
+		grid-template-rows: 40px 2fr;
+		grid-template-areas: 'exphdr'
+		'expcontent';
+		grid-gap: 4px;
+		margin: 10px auto;
+		font-size: 14px;
+	}
+	.exp-hdr-title {
+		border-bottom: 1px solid #cccccc;
+		padding: 8px;
+	}
+	
+	.exphdr {
+		padding: 8px;
+		box-shadow: 0 0 4px #999999;
+	}
+	.exp-detail {
+		padding: 10px;
+	}
+	.expcontent {
+		grid-area: expcontent;
+		padding: 8px;
+		font-size: 16px;
+		transition: all 1s;
+		height: 60vh;
+		overflow-y: auto;
+		background-color: #def;
+	}
+
+	@media screen and (min-width: 512px) {
+		.expcontent {
+			height: 60vh;
+			overflow-y: auto;
+		}
 	}
 </style>
