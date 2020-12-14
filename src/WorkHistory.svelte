07a0f3d5de1f52e1a605d2c345a0121ff7d1c73e
srikantgudi <srikantgudi@gmail.com>
2020-12-14 07:34:19 +0530

    Update WorkHistory.svelte

--- src/WorkHistory.svelte
+++ src/WorkHistory.svelte
@@ -1,126 +1,125 @@
 <script>
-import Panel from "./Panel.svelte";
-    let expList = [
-		{
-			header: {
-				dates: '16-Jun-2016 to present',
-				jobTitle: 'Project Lead - System',
-				org: 'Mphasis, Bengaluru',
-			},
-			content: [
-                'Currently working as Lead developer in Design System with Schneider Electric',
-				'Worked offshore in Bengaluru with a banking conglomerate on  projects in Angular and React',
-				'Worked onsite in Glasgow, UK with a banking conglomerate on Angular based projects',
-				'Developed UI for a POC for Digital Readiness Analysis Tool to create a survey questionnaire for various categories using Angular, Material Design, HTML5, CSS3, JavaScript, API integration',
-				'Developed UI for client in Insurance domain using React, Material-UI, HTML5, CSS3, JavaScript, IndexedDB',
-				'Worked offshore at client-side with Schneider Electric on Management Dashboard using Angular 1.x',
-			]
-		},
-		{
-			header: {
-				dates: '15-Aug-2015 to 14-Jan-2016',
-				jobTitle: 'Senior AngularJS Developer',
-				org: 'Pandera Solutions India Pvt Ltd, Bengaluru',
-			},
-			content: [
-				'Worked in Angular on various projects',
-				'Created sub-components for re-use',
-				'Interacted with client-side developers to understand the requirements',
-				'Integrated APIs in the frontend code'
-			]
-		},
-		{
-			header: {
-				dates: '20-Aug-2014 to 22-Jan-2015',
-				jobTitle: 'Senior Software Developer',
-				org: 'Basil Communications Pvt Ltd, Bengaluru',
-			},
-			content: [
-				'Created HTML mockups for various applications',
-				'Interacted with designers to understand the designs',
-				'Used Photoshop to slice images'
-			]
-		},
-		{
-			header: {
-				dates: 'Apr-2014 to Jun-2014',
-				jobTitle: 'Senior AngularJS Developer (Remote work)',
-				org: 'Satsyil, VA, USA',
-			},
-			content: [
-				'Worked remotely as senior UI developer using AngularJS technology',
-				'Modified admin-side modules and screen layouts and created new ones as required'
-			]
-		},
-		{
-			header: {
-				dates: '23-Nov-2011 – 07-Mar-2014',
-				jobTitle: 'Senior Software Developer (Consultant)',
-				org: 'Tejas Systems / SUNSYSTEMS, Bengaluru :: Client: MModal Global Services Pvt Ltd',
-			},
-			content: [
-				'Environment: PHP, MySQL, Jquery, Fancybox, Fusioncharts, HTML, CSS (Bootstrap), AngularJS',
-				'modified client-side and admin-side scripts',
-				'created animated presentation of iPad application using Jquery and Jquery-cycle',
-				'created presentation of mobile application in Powerpoint',
-				'created UI for password management using AngularJS'
-			]
-		},
-		{
-			header: {
-				dates: '10-Feb-2010 – 22-Sep-2011',
-				jobTitle: 'Senior Software Engineer',
-				org: 'TechJini Solutions Pvt Ltd, Bengaluru',
-			},
-			content: [
-				'Environment: MySQL, PHP, HTML, CSS, Jquery, amChart',
-				'Projects worked on:',
-				'Coupons System (MySql, PHP, HTML, CSS, Jquery, HighCharts)',
-				'eLearning System (PHP [Symfony], MySql, HTML, CSS, Jquery, amCharts): Developed Quiz module. Developed reports including one using amCharts.',
-				'Contest based application (PHP [Symfony], MySql, HTML, CSS, Jquery)'
-			],	
-			{
-				header: {
-					dates: '25-Jul-2007 – 13-Mar-2009',
-					jobTitle: 'Senior Software Engineer',
-					org: 'TLI Software Pvt Ltd, Bengaluru',
-				},
-				content: [
-					'Environment:​ PHP, MySQL, HTML, CSS, Javascript, Jquery',
-					'Worked​ as Full-time resource (3 months on-site and 3 months offsite), with Computer Methods I​nternational Corp, Toronto, Canada (www.cmic.ca), on JasperReports.',
-					'Developed web applications independently as per client requirements using PHP, MySql'
-				],
-			}
-	];
-	let curExp = expList[0];
+  import Panel from './Panel.svelte'
+  let expList = [
+    {
+      header: {
+        dates: '16-Jun-2016 to present',
+        jobTitle: 'Project Lead - System',
+        org: 'Mphasis, Bengaluru',
+      },
+      content: [
+        'Currently working as Lead developer in Design System with Schneider Electric',
+        'Worked offshore in Bengaluru with a banking conglomerate on  projects in Angular and React',
+        'Worked onsite in Glasgow, UK with a banking conglomerate on Angular based projects',
+        'Developed UI for a POC for Digital Readiness Analysis Tool to create a survey questionnaire for various categories using Angular, Material Design, HTML5, CSS3, JavaScript, API integration',
+        'Developed UI for client in Insurance domain using React, Material-UI, HTML5, CSS3, JavaScript, IndexedDB',
+        'Worked offshore at client-side with Schneider Electric on Management Dashboard using Angular 1.x',
+      ],
+    },
+    {
+      header: {
+        dates: '15-Aug-2015 to 14-Jan-2016',
+        jobTitle: 'Senior AngularJS Developer',
+        org: 'Pandera Solutions India Pvt Ltd, Bengaluru',
+      },
+      content: [
+        'Worked in Angular on various projects',
+        'Created sub-components for re-use',
+        'Interacted with client-side developers to understand the requirements',
+        'Integrated APIs in the frontend code',
+      ],
+    },
+    {
+      header: {
+        dates: '20-Aug-2014 to 22-Jan-2015',
+        jobTitle: 'Senior Software Developer',
+        org: 'Basil Communications Pvt Ltd, Bengaluru',
+      },
+      content: [
+        'Created HTML mockups for various applications',
+        'Interacted with designers to understand the designs',
+        'Used Photoshop to slice images',
+      ],
+    },
+    {
+      header: {
+        dates: 'Apr-2014 to Jun-2014',
+        jobTitle: 'Senior AngularJS Developer (Remote work)',
+        org: 'Satsyil, VA, USA',
+      },
+      content: [
+        'Worked remotely as senior UI developer using AngularJS technology',
+        'Modified admin-side modules and screen layouts and created new ones as required',
+      ],
+    },
+    {
+      header: {
+        dates: '23-Nov-2011 – 07-Mar-2014',
+        jobTitle: 'Senior Software Developer (Consultant)',
+        org:
+          'Tejas Systems / SUNSYSTEMS, Bengaluru :: Client: MModal Global Services Pvt Ltd',
+      },
+      content: [
+        'Environment: PHP, MySQL, Jquery, Fancybox, Fusioncharts, HTML, CSS (Bootstrap), AngularJS',
+        'modified client-side and admin-side scripts',
+        'created animated presentation of iPad application using Jquery and Jquery-cycle',
+        'created presentation of mobile application in Powerpoint',
+        'created UI for password management using AngularJS',
+      ],
+    },
+    {
+      header: {
+        dates: '10-Feb-2010 – 22-Sep-2011',
+        jobTitle: 'Senior Software Engineer',
+        org: 'TechJini Solutions Pvt Ltd, Bengaluru',
+      },
+      content: [
+        'Environment: MySQL, PHP, HTML, CSS, Jquery, amChart',
+        'Projects worked on:',
+        'Coupons System (MySql, PHP, HTML, CSS, Jquery, HighCharts)',
+        'eLearning System (PHP [Symfony], MySql, HTML, CSS, Jquery, amCharts): Developed Quiz module. Developed reports including one using amCharts.',
+        'Contest based application (PHP [Symfony], MySql, HTML, CSS, Jquery)',
+      ],
+    },
+    {
+      header: {
+        dates: '25-Jul-2007 – 13-Mar-2009',
+        jobTitle: 'Senior Software Engineer',
+        org: 'TLI Software Pvt Ltd, Bengaluru',
+      },
+      content: [
+        'Environment:​ PHP, MySQL, HTML, CSS, Javascript, Jquery',
+        'Worked​ as Full-time resource (3 months on-site and 3 months offsite), with Computer Methods I​nternational Corp, Toronto, Canada (www.cmic.ca), on JasperReports.',
+        'Developed web applications independently as per client requirements using PHP, MySql',
+      ],
+    },
+  ]
+  let curExp = expList[0]
 </script>
 
 <style>
+
 </style>
 
 <Panel>
-	<div slot="header">
-		Work Experience
-	</div>
-	<div slot="content">
-		{#each expList as exp}
-		<Panel hdrbg="#333333" showcontent={curExp === exp}>
-			<div slot="header" on:click={() => curExp = exp}>
-				<div class="sectiontitle">
-					{exp.header.dates} | {exp.header.jobTitle}
-				</div>
-				<div class="subtitle">
-					{exp.header.org}
-				</div>
-			</div>
-			<div slot="content">
-				<ul>
-					{#each curExp.content as detail}
-					<li>{detail}</li>
-					{/each}
-				</ul>
-			</div>
-		</Panel>
-		{/each}
-	</div>
+  <div slot="header">Work Experience</div>
+  <div slot="content">
+    {#each expList as exp}
+      <Panel hdrbg="#333333" showcontent={curExp === exp}>
+        <div slot="header" on:click={() => (curExp = exp)}>
+          <div class="sectiontitle">
+            {exp.header.dates} | {exp.header.jobTitle}
+          </div>
+          <div class="subtitle">{exp.header.org}</div>
+        </div>
+        <div slot="content">
+          <ul>
+            {#each curExp.content as detail}
+              <li>{detail}</li>
+            {/each}
+          </ul>
+        </div>
+      </Panel>
+    {/each}
+  </div>
 </Panel>
