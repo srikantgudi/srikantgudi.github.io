4c37be072e6d614e325f2d6a80e3f068293f7922
srikantgudi <srikantgudi@gmail.com>
2020-12-17 01:16:36 +0530

    Modified styles

--- src/Topnav.svelte
+++ src/Topnav.svelte
@@ -5,7 +5,14 @@
         <div class="contact-item">srikantgudi@gmail.com</div>
         <div class="contact-item">+918296656336</div>
       </div>
-      <div class="pos">
-          Senior Frontend Professional
+      <div>
+        <svg height="30" width="auto">
+          <text x="50%" y="20" class="pos" text-anchor="middle">
+            Senior Frontend Professional
+            <animate attributeName="fill"
+              values="blue;lightblue;navy;lightblue;blue"
+              begin="0" dur="10s" repeatCount="indefinite" />
+          </text>
+        </svg>
       </div>
 </div>
