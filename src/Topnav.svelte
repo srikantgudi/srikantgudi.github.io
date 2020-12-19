e76e8431b1b3cbc85557e81f02084120f330da6c
srikantgudi <srikantgudi@gmail.com>
2020-12-20 04:43:57 +0530

    Moved the email label to left.

--- src/Topnav.svelte
+++ src/Topnav.svelte
@@ -1,8 +1,8 @@
 <div class="topnav">
       <div class="pageTitle">Srikant Gudi</div>
       <div class="contact">
-        <div class="contact-item">Bengaluru, India</div>
         <div class="contact-item">srikantgudi@gmail.com</div>
+        <div class="contact-item">Bengaluru, India</div>
         <div class="contact-item">+918296656336</div>
       </div>
       <div>
