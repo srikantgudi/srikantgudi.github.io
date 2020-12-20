03f50935820f07fd106728b4903aced05c16d5bf
srikantgudi <srikantgudi@gmail.com>
2020-12-20 23:19:05 +0530

    Modified content in profile.

--- src/Profile.svelte
+++ src/Profile.svelte
@@ -7,10 +7,22 @@
             Senior software professional with over 20 years of experience in various technologies
         </li>
         <li>
-            From April 2014 into frontend development using Angular, Svelte, Stencil, React, VueJS, NodeJS
+            April 2014 onwards: Lead Front-end developer using Angular, Svelte, Stencil, React, NodeJS
+        </li>
+        <li>
+            Nov 2005 to March 2014: into Web development using PHP, MySQL, JQuery, JavaScript, HTML, CSS
+        </li>
+        <li>
+            Prior to Nov 2005: Application development using FoxPro, Visual Basic, PowerBuilder
         </li>
         <li>
             Can create POCs, presentations, and wireframes.
+        </li>
+        <li>
+          Have knowledge of Web components development, VueJS, NuxtJS, NextJS, MongoDB
+        </li>
+        <li>
+          Have knowledge of Git, Github Desktop, Gist, BitBucket, Confluence, TortoiseGit
         </li>
     </ul>
 </div>
