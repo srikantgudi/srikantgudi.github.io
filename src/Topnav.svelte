6aa62307bfb13f8f020fc50c9112b67df787cd31
srikantgudi <srikantgudi@gmail.com>
2020-12-20 05:42:39 +0530

    Made changes to layout.

--- src/Topnav.svelte
+++ src/Topnav.svelte
@@ -1,20 +1,18 @@
 <div class="topnav">
-  <div>
-    <div class="pageTitle">Srikant Gudi</div>
-    <div>Bengaluru, India</div>
-  </div>
-  <div>
-    <div>srikantgudi@gmail.com</div>
-    <div>+91-829-665-6336</div>
-  </div>
-</div>
-<div class="pos">
-  <svg height="30" width="auto">
-    <text x="50%" y="20" class="pos" text-anchor="middle">
-      Senior Frontend Professional
-      <animate attributeName="fill"
-        values="blue;lightblue;navy;lightblue;blue"
-        begin="0" dur="10s" repeatCount="indefinite" />
-    </text>
-  </svg>
+      <div class="pageTitle">Srikant Gudi</div>
+      <div class="contact">
+        <div class="contact-item">Bengaluru, India</div>
+        <div class="contact-item">srikantgudi@gmail.com</div>
+        <div class="contact-item">+91-829-665-6336</div>
+      </div>
+      <div>
+        <svg height="30" width="auto">
+          <text x="50%" y="20" class="pos" text-anchor="middle">
+            Senior Frontend Professional
+            <animate attributeName="fill"
+              values="blue;lightblue;navy;lightblue;blue"
+              begin="0" dur="10s" repeatCount="indefinite" />
+          </text>
+        </svg>
+      </div>
 </div>
