b68ded115f3f7f97e968beaf637890f130b6dce2
srikantgudi <srikantgudi@gmail.com>
2020-12-21 00:53:29 +0530

    Fixed the style.

--- src/Skills.svelte
+++ src/Skills.svelte
@@ -45,37 +45,25 @@
 	}
 </script>
 
-<style>
-  dl.skills {
-    display: flex;
-    flex-direction: column;
-    width: 20em;
-    box-shadow: 0 2px 4px #999999;
-    margin-bottom: 2em;
-  }
-dt {
-  font-weight: bold;
-  background-color: skyblue;
-  padding: 4px;
-}
-li {
-  list-style: square;
-}
-</style>
 
-<dl class="skills">
-	{#each skills as skill}
-		<dt>{skill.group}</dt>
-		<dd>
-			{#each skill.items as item}
-				<li>
-					<svg height="30" width="100">
-						<rect x="0" y="0" height="100%" width="100%" fill="lightgrey"/>
-						<rect x="0" y="0" height="100%" width={item.rating * 10} fill="grey"/>
-					</svg>
-					{item.name}
-			</li>
-			{/each}
-		</dd>
-	{/each}
-</dl>
+<div class="section">
+    <div class="sectiontitle">
+        Technical Skills
+    </div>
+    <dl class="skillgroups">
+      {#each skills as skill}
+        <dt class="groupname">{skill.group}</dt>
+        <dd>
+          {#each skill.items as item}
+            <li>
+              <svg height="30" width="100">
+                <rect x="0" y="0" height="100%" width="100%" fill="lightgrey"/>
+                <rect x="0" y="0" height="100%" width={item.rating * 10} fill="grey"/>
+              </svg>
+              {item.name}
+          </li>
+          {/each}
+        </dd>
+      {/each}
+    </dl>
+</div>
