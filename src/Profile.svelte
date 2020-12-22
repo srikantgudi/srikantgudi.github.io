4b35e1b62fd1f5d655cf872f2183bb79ae219d8a
srikantgudi <srikantgudi@gmail.com>
2020-12-23 00:44:36 +0530

    Update Profile.svelte

--- src/Profile.svelte
+++ src/Profile.svelte
@@ -1,16 +1,35 @@
 <div class="section">
-    <div class="sectiontitle">
-        Profile
-    </div>
-    <ul>
-        <li>
-            Senior software professional with over 20 years of experience in various technologies
-        </li>
-        <li>
-            From April 2014 into frontend development using Angular, Svelte, Stencil, React, VueJS, NodeJS
-        </li>
-        <li>
-            Can create POCs, presentations, and wireframes.
-        </li>
-    </ul>
+  <div class="sectiontitle">Profile</div>
+  <ul>
+    <li>
+      Senior software professional with over 20 years of experience in various
+      technologies
+    </li>
+    <li>
+      From April 2014 into frontend development using Angular, Svelte, Stencil,
+      React, VueJS, NodeJS
+    </li>
+    <li>Can create POCs, presentations, and wireframes.</li>
+  </ul>
+  <div class="sectiontitle">Academic Qualifications:</div>
+  <ul>
+    <li>Master in Computer Management</li>
+    <li>Bachelor of Commerce</li>
+  </ul>
+  <div class="sectiontitle">Certifications:</div>
+  <ul>
+    <li>Brainbench Certifications in C++, COBOL, and Visual Basic</li>
+    <li>
+      <a
+        href="https://www.hackerrank.com/certificates/24b0d06ab4d8"
+        target="_blank">
+        HackerRank certification in Python (Basic)
+      </a>
+      <a
+        href="https://www.hackerrank.com/certificates/5371896c1aab"
+        target="_blank">
+        HackerRank certification in Python (Intermediate)
+      </a>
+    </li>
+  </ul>
 </div>
