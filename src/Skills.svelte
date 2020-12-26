b9f33a1dd83c9682453eb2eb2abc69fcdd71097f
srikantgudi <srikantgudi@gmail.com>
2020-12-26 12:54:09 +0530

    Rendered skills as a svg image

--- src/Skills.svelte
+++ src/Skills.svelte
@@ -1,6 +1,21 @@
 <script>
-    import Rategraph from './Rategraph.svelte';
-
+    import Sklevel from './Sklevel.svelte';
+		let name = 'Technical Skills';
+		let skills = [
+			{name: 'Angular', level: 8},
+			{name: 'Svelte', level: 7},
+			{name: 'VueJS', level: 7},
+			{name: 'React', level: 7},
+			{name: 'Stencil', level: 6},
+			{name: 'NodeJS', level: 6},
+			{name: 'JavaScript', level: 8},
+			{name: 'HTML5', level: 8},
+			{name: 'CSS3', level: 7},
+			{name: 'MySQL', level: 7},
+			{name: 'MongoDB', level: 6},
+			{name: 'SVG', level: 5},
+			{name: 'Python', level: 4},
+		];
 </script>
 
 
@@ -8,31 +23,7 @@
     <div class="sectiontitle">
         Technical Skills
     </div>
-    <dl class="skillgroups">
-      <svg height="400" width="400">
-				<circle r="30" cx="65" cy="70" fill="#dde" />
-				<text x="35" y="75" font-size="15">Angular</text>
-				<circle r="18" cx="120" cy="70" fill="lightblue" />
-				<text x="105" y="75" font-size="11" font-weight="300">React</text>
-				<circle r="22" cx="170" cy="70" fill="#def" />
-				<text x="156" y="75" font-size="14" font-weight="600" fill="blue">Vue</text>
-				<circle r="22" cx="218" cy="70" fill="#def" />
-				<text x="195" y="75" font-size="14" font-weight="600" fill="blue">Svelte</text>
-				<circle r="18" cx="270" cy="70" fill="#def" />
-				<text x="250" y="75" font-size="12" font-weight="600" fill="blue">Stencil</text>
-				<circle r="30" cx="65" cy="140" fill="#def" />
-				<text x="28" y="145" font-size="14" font-weight="600" fill="blue">JavaScript</text>
-				<circle r="25" cx="132" cy="140" fill="#def" />
-				<text x="108" y="145" font-size="14" font-weight="600" fill="blue">HTML5</text>
-				<circle r="25" cx="190" cy="140" fill="#def" />
-				<text x="170" y="145" font-size="14" fill="blue" font-weight="600">CSS3</text>
-				<circle r="25" cx="250" cy="140" fill="lightblue" />
-				<text x="228" y="145" font-size="12" font-weight="600" fill="blue">NodeJS</text>
-				
-				<circle r="18" cx="122" cy="200" fill="lightblue" />
-				<text x="100" y="205" font-size="14">MySQL</text>
-				<ellipse rx="20" ry="15" cx="180" cy="200" fill="#ddd" />
-				<text x="168" y="205" font-size="16">Git</text>
-			</svg>
-    </dl>
+    {#each skills as skill}
+			<Sklevel text={skill.name} level={skill.level} />
+		{/each}
 </div>
