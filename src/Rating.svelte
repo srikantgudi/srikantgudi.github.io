f98001101013abafaad18b6024e8657ba57b7e09
srikantgudi <srikantgudi@gmail.com>
2020-12-26 14:01:41 +0530

    Modified color codes for rating skills

---
+++ src/Rating.svelte
@@ -0,0 +1,53 @@
+<script>
+  export let text = 'testing text'
+  export let level = 1
+  let levels = [
+    '5%',
+    '15%',
+    '25%',
+    '35%',
+    '45%',
+    '55%',
+    '65%',
+    '75%',
+    '85%',
+    '95%',
+  ]
+  let clr = [
+    'aliceblue',
+    'lightyellow',
+    'lightcyan',
+    'lightblue',
+    'teal',
+    'goldenrod',
+    'orange',
+    'brown',
+    '#900',
+    'maroon',
+  ]
+</script>
+
+<svg height="50" width="140" style="margin:2px 4px;box-shadow: 0 0 2px #666">
+  {#each levels as lvl, l}
+    <rect
+      x={l * 14 + 1}
+      y="1"
+      width="20"
+      height="18"
+      stroke="grey"
+      fill={clr[l]} />
+    <text x={l * 14 + 5} y="14" font-size="8" fill={l < 7 ? '#000' : '#f9f9f9'}>
+      {(l + 1).toString()}
+    </text>
+  {/each}
+  <text
+    x="70"
+    y="47"
+    font-size="10"
+    font-family="Comic Sans MS"
+    text-anchor="middle">
+    {text}
+  </text>
+  <circle r="2" cx="70" cy="35" />
+  <line x1="70" y1="35" x2={levels[level - 1]} y2="25" stroke="#666" />
+</svg>
--- src/Skills.svelte
+++ src/Skills.svelte
@@ -1,29 +1,26 @@
 <script>
-    import Sklevel from './Sklevel.svelte';
-		let name = 'Technical Skills';
-		let skills = [
-			{name: 'Angular', level: 8},
-			{name: 'Svelte', level: 7},
-			{name: 'VueJS', level: 7},
-			{name: 'React', level: 7},
-			{name: 'Stencil', level: 6},
-			{name: 'NodeJS', level: 6},
-			{name: 'JavaScript', level: 8},
-			{name: 'HTML5', level: 8},
-			{name: 'CSS3', level: 7},
-			{name: 'MySQL', level: 7},
-			{name: 'MongoDB', level: 6},
-			{name: 'SVG', level: 5},
-			{name: 'Python', level: 4},
-		];
+  import Rating from './Rating.svelte'
+  let name = 'Technical Skills'
+  let skills = [
+    { name: 'Angular', level: 8 },
+    { name: 'Svelte', level: 7 },
+    { name: 'VueJS', level: 7 },
+    { name: 'React', level: 7 },
+    { name: 'Stencil', level: 6 },
+    { name: 'NodeJS', level: 6 },
+    { name: 'JavaScript', level: 8 },
+    { name: 'HTML5', level: 8 },
+    { name: 'CSS3', level: 7 },
+    { name: 'MySQL', level: 7 },
+    { name: 'MongoDB', level: 6 },
+    { name: 'SVG', level: 5 },
+    { name: 'Python', level: 4 },
+  ]
 </script>
 
-
 <div class="section">
-    <div class="sectiontitle">
-        Technical Skills
-    </div>
-    {#each skills as skill}
-			<Sklevel text={skill.name} level={skill.level} />
-		{/each}
+  <div class="sectiontitle">Technical Skills</div>
+  {#each skills as skill}
+    <Rating text={skill.name} level={skill.level} />
+  {/each}
 </div>
