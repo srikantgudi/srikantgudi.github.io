f9ef9ee53c7ad4be5f30778d38ec94e0cb9a08b0
srikantgudi <srikantgudi@gmail.com>
2021-03-13 17:33:50 +0530

    Modified rating display.

---
+++ src/SkillLevel.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let rating = 50;
+	export let label = "HTML";
+</script>
+<style>
+	.wrapper {
+		display:flex;
+		flex-flow:column;
+		align-items:flex-start;
+		background-color: lightcyan;
+		width:205px;
+		padding: 2px;
+		margin-bottom: 4px;
+	}
+</style>
+<div class="wrapper">
+	<div>
+		{label}
+	</div>
+	<div>
+		<svg height="40" width="200">
+			<rect x="0" y="0" width="100%" height="100%" fill="lightblue" stroke="navy" />
+			<rect x="1%" y="10%" width={`${rating}%`} height="80%" fill="navy" />
+		</svg>		
+	</div>
+</div>
+
--- src/Skills.svelte
+++ src/Skills.svelte
@@ -1,42 +1,40 @@
 <script>
-  import {mainOpt} from './store';
-  import Panel from './Panel.svelte';
-  import Rating from './Rating.svelte';
+  import SkillLevel from './SkillLevel.svelte';
   
   let name = 'Technical Skills'
   let skills = [
     {
       group: 'Frameworks',
       items: [
-        { name: 'Angular', level: 8 },
-        { name: 'Svelte', level: 7 },
-        { name: 'VueJS', level: 7 },
-        { name: 'React', level: 7 },
-        { name: 'Stencil', level: 6 },
+        { name: 'Svelte', level: 85 },
+        { name: 'Angular', level: 80 },
+        { name: 'VueJS', level: 80 },
+        { name: 'React', level: 75 },
+        { name: 'Stencil', level: 70 },
       ],
     },
     {
       group: 'Web Skills',
       items: [
-        { name: 'JavaScript', level: 8 },
-        { name: 'HTML5', level: 8 },
-        { name: 'CSS3', level: 7 },
-        { name: 'NodeJS', level: 6 },
-        { name: 'SVG', level: 5 },
+        { name: 'JavaScript', level: 85 },
+        { name: 'HTML5', level: 85 },
+        { name: 'CSS3', level: 85 },
+        { name: 'NodeJS', level: 75 },
+        { name: 'SVG', level: 75 },
       ],
     },
     {
       group: 'Databases/ NoSQL',
       items: [
-        { name: 'MySQL', level: 7 },
-        { name: 'MongoDB', level: 6 },
+        { name: 'MySQL', level: 75 },
+        { name: 'MongoDB', level: 65 },
       ],
     },
     {
       group: 'Others',
       items: [
-        { name: 'Django', level: 5 },
-        { name: 'Python', level: 4 },
+        { name: 'Django', level: 60 },
+        { name: 'Python', level: 55 },
       ],
     },
   ]
@@ -44,6 +42,9 @@
 
 <style>
   .skillgroup {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
     padding: 1vh 2vw;
     box-shadow: 0 2px 4px #999999;
     margin-bottom: 10px;
@@ -57,7 +58,7 @@
       <div class="sectionsubtitle">{skill.group}</div>
       <div class="skillgroup">
         {#each skill.items as item}
-          <Rating text={item.name} level={item.level} />
+        <SkillLevel label={item.name} rating={item.level} />
         {/each}
       </div>
     </div>
