4bbb2e10b12a63166248b1093eb9c4bfecfa007b
srikantgudi <srikantgudi@gmail.com>
2021-03-13 21:10:51 +0530

    Modified skills layout

--- src/SkillLevel.svelte
+++ src/SkillLevel.svelte
@@ -18,9 +18,9 @@
 		{label}
 	</div>
 	<div>
-		<svg height="40" width="200">
-			<rect x="0" y="0" width="100%" height="100%" fill="lightblue" stroke="navy" />
-			<rect x="1%" y="10%" width={`${rating}%`} height="80%" fill="navy" />
+		<svg height="20" width="200">
+			<rect x="0" y="0" rx="3" ry="3" width="100%" height="100%" fill="lightblue" stroke="navy" />
+			<rect x="1%" y="10%" rx="5" width={`${rating}%`} height="80%" fill="navy" />
 		</svg>		
 	</div>
 </div>
--- src/Skills.svelte
+++ src/Skills.svelte
@@ -3,64 +3,60 @@
   
   let name = 'Technical Skills'
   let skills = [
-    {
-      group: 'Frameworks',
-      items: [
-        { name: 'Svelte', level: 85 },
-        { name: 'Angular', level: 80 },
-        { name: 'VueJS', level: 80 },
-        { name: 'React', level: 75 },
-        { name: 'Stencil', level: 70 },
-      ],
-    },
-    {
-      group: 'Web Skills',
-      items: [
-        { name: 'JavaScript', level: 85 },
-        { name: 'HTML5', level: 85 },
-        { name: 'CSS3', level: 85 },
-        { name: 'NodeJS', level: 75 },
-        { name: 'SVG', level: 75 },
-      ],
-    },
-    {
-      group: 'Databases/ NoSQL',
-      items: [
-        { name: 'MySQL', level: 75 },
-        { name: 'MongoDB', level: 65 },
-      ],
-    },
-    {
-      group: 'Others',
-      items: [
-        { name: 'Django', level: 60 },
-        { name: 'Python', level: 55 },
-      ],
-    },
+    // {
+    //   group: 'Frameworks',
+    //   items: [
+    //     { name: 'Svelte', level: 85 },
+    //     { name: 'Angular', level: 80 },
+    //     { name: 'VueJS', level: 80 },
+    //     { name: 'React', level: 75 },
+    //     { name: 'Stencil', level: 70 },
+    //   ],
+    // },
+    // {
+    //   group: 'Web Skills',
+    //   items: [
+    //     { name: 'JavaScript/TypeScript', level: 85 },
+    //     { name: 'HTML5', level: 85 },
+    //     { name: 'CSS3', level: 85 },
+    //     { name: 'NodeJS', level: 75 },
+    //     { name: 'SVG', level: 75 },
+    //   ],
+    // },
+    // {
+    //   group: 'Databases/ NoSQL',
+    //   items: [
+    //     { name: 'MySQL', level: 75 },
+    //     { name: 'MongoDB', level: 65 },
+    //   ],
+    // },
+    // {
+    //   group: 'Others',
+    //   items: [
+    //     { name: 'Django', level: 60 },
+    //     { name: 'Python', level: 55 },
+    //   ],
+    // },
+
+    {group: 'Frameworks', items: 'Anguiar, Svelte, VueJS, React, Stencil'},
+    {group: 'Web Skills', items: 'JavaScript/TypeScript, HTML5, CSS3, SVG'},
+    {group: 'Libraries', items: 'Bootstrap, Angular-Material, React-UI, Vuetify, Buefy'},
+    {group: 'Agile Methodology', items: 'Scrum, Sprint, JIRA, GIT, Confluence'},
+    {group: 'Operating Systems', items: 'Windows, MacOS, Linux (Ubuntu, Mint)'},
+    {group: 'Coding Tools', items: 'VS Code, IntelliJ, Nodepad++'},
+    {group: 'Others', items: 'Python-Django, Cansandra'},
+    {group: 'Online coding', items: 'StackBlitz, CodeSandbox'},
   ]
 </script>
 
-<style>
-  .skillgroup {
-    display: flex;
-    flex-flow: row wrap;
-    align-items: center;
-    padding: 1vh 2vw;
-    box-shadow: 0 2px 4px #999999;
-    margin-bottom: 10px;
-  }
-</style>
-
 <div class="sectiontitle">Technical Skills</div>
 <div class="inner-content-body">
-  {#each skills as skill}
-    <div>
-      <div class="sectionsubtitle">{skill.group}</div>
+  <div class="skills">
+    {#each skills as skill}
       <div class="skillgroup">
-        {#each skill.items as item}
-        <SkillLevel label={item.name} rating={item.level} />
-        {/each}
+        <div class="grouptitle">{skill.group}:</div>
+        <div>{skill.items}</div>
       </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </div>
