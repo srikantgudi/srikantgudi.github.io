07d891f236755c8815cd64c10e31fc5e7c989fe0
srikantgudi <srikantgudi@gmail.com>
2021-03-18 02:22:04 +0530

    Modified layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,221 @@
 <script>
-  import Topnav from "./Topnav.svelte";
-  import Profile from "./Profile.svelte";
-  import Skills from "./Skills.svelte";
-  import WorkHistory from "./WorkHistory.svelte";
-
-  let components = [
-    {key: 'profile', name: 'Profile', comp: Profile},
-    {key: 'skills', name: 'Technical Skills', comp: Skills},
-    {key: 'exp', name: 'Work History', comp: WorkHistory}
-  ]
-  let currComp = components[0].comp;
-  let curIdx = 0;
-
-  const goComp = (delta) => {
-    curIdx += delta;
-    if (curIdx < 0) {
-      curIdx = components.length - 1;
-    } else if (curIdx > (components.length -1)) {
-      curIdx = 0;
-    }
-    currComp = components[curIdx].comp;
-  }
+	import { data } from './data/data.js'
 </script>
-
-<div id="app">
-  <Topnav />
-  <div class="menubar">
-    {#each components as component}
-    <button class="menuitem" class:active={currComp === component.comp} on:click={() => {currComp = component.comp}}>
-      {component.name}
-    </button>
-    {/each}
-  </div>
-  <div class="content">
-    <svelte:component this={currComp} />
-  </div>
+<style>
+	#app {
+		width: 60vw;
+    height: auto;
+		margin: 0 auto;
+		padding: 10px;
+		background: linear-gradient(lightblue,lightcyan,lightblue);
+		color: navy;
+		line-height: 1.2;
+	}
+	.info {
+		display: flex;
+		flex-flow: column;
+		box-shadow: 0 2px 4px #999;
+		padding: 10px;
+		border-radius: 1em 1em 0 0;
+	}
+	.grad-lite {
+		background: linear-gradient(lightcyan,lightblue);
+		color: navy;
+	}
+	.grad-dark {
+		background: linear-gradient(grey, darkslategrey);
+		color: whitesmoke;
+	}
+	.round-border-top {
+		border-radius: 10px 10px 0 0;
+	}
+	.pagetitle {
+		height: 3em;
+		background: linear-gradient(lightblue,blue);
+		color: whitesmoke;
+		border-radius: 1em;
+		text-align: center;
+		padding: 10px;
+	}
+	.name {
+    font-family: Righteous;
+    letter-spacing: 1px;
+		font-weight: 600;
+		text-transform: uppercase;
+		font-size: 24px;
+	}
+	.positiontitle {
+		font-size: 18px;
+		font-family: Orbitron;
+		font-weight: 600;
+		padding: 10px;
+		margin: 0 4vw;
+		background: linear-gradient(lightblue,blue);
+		color: whitesmoke;
+		border-radius: 1em;
+	}
+	.space-around {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+	.sectiontitle {
+		margin-top: 10px;
+		box-sizing: border-box;
+		font-size: 18px;
+		font-family: Montserrat;
+		font-weight: 600;
+		color: navy;
+		padding: 10px;
+		background-color: lightblue;
+		border-radius: 20px 20px 0 0;
+	}
+  .profile {
+    font-family: Roboto;
+    font-size: 14px;
+  }
+	.contacts {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.contact-item {
+		min-width: 30%;
+		text-align: center;
+	}
+	.box {
+		box-shadow: 0 2px 20px #999999;
+		border-radius: 0 0 10px 10px
+	}
+	.list {
+		padding: 10px 20px;
+	}
+	.edu {
+		font-size: 12px;
+	}
+	.skills {
+    font-family: Roboto;
+    font-size: 14px;
+	}
+	.exp {
+		display: flex;
+		flex-flow: column;
+		padding: 10px;
+	}
+  .jobtitle {
+    font-family: Montserrat;
+    background-color: darkslategrey;
+    padding: 4px 10px;
+    color: whitesmoke;
+    border-radius: 10px 10px 0 0;
+  }
+	.expdetail {
+		padding: 10px;
+		text-align: center;
+    font-family: Roboto;
+	}
+	@media screen and (max-width: 840px) {
+		.pagetitle {
+			height: auto;
+			padding: 10px;
+		}
+    .sectiontitle, .subsectiontitle {
+      font-size: 90%;
+    }
+		.name {
+			font-size: 80%;
+		}
+		.edu-list, .list {
+			width: 100%;
+			font-size: 12px;
+			padding: 0;
+		}
+		.contacts {
+			flex-flow: column;
+			font-size: 70%;
+		}
+		.positiontitle {
+			font-size: 8px;
+			letter-spacing: 0;
+		}
+	}
+</style>
+<div id="app" class="grad-app">
+	<div class="info grad-lite">
+		<div class="pagetitle">
+			<div class="name">
+				Srikant Gudi
+			</div>
+			<div class="edu-list">
+				<span class="edu" title="Master in Computer Management">M.C.M.</span>
+				<span class="edu" title="Bachelor of Commerce">B.Com.</span>
+			</div>
+		</div>
+		<div>
+			
+		</div>
+		<div class="contacts">
+			<div class="contact-item">
+				Bengaluru, India
+			</div>
+			<div class="contact-item">
+				srikantgudi@gmail.com
+			</div>
+			<div class="contact-item">
+				+91 829 665 6336
+			</div>
+		</div>
+		<div class="positiontitle space-around">
+			SENIOR FRONTEND ENGINEER
+		</div>
+	</div>
+	<div>
+		<div class="sectiontitle grad-lite">
+			PROFILE
+		</div>
+		<div class="box profile">
+			<ul>
+				{#each data.profile as text}
+				<li>{text}</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+	<div>
+		<div class="sectiontitle grad-lite">
+			TECHNICAL SKILLS
+		</div>
+		<div class="box list">
+			<div class="skills">
+				<ul>
+					{#each data.skills as text}
+					<li>{text}</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	<div>
+		<div class="sectiontitle grad-lite">
+			WORK EXPERIENCE
+		</div>
+		<div class="box list">
+			{#each data.exp as item}
+				<div class="exp box">
+					<div class="jobtitle">
+						{item.jobTitle} - {item.dates}
+					</div>
+					<div class="expdetail">
+						<div>
+							{item.org}
+						</div>
+						<div>
+							Technologies used: {item.technology}
+						</div>	
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
 </div>
---
+++ src/data/data.js
@@ -0,0 +1,74 @@
+export const data = {
+	profile: [
+		"Senior software professional with over 20 years of experience in various technologies",
+		"From April 2014 into frontend development using Angular, Svelte, Stencil, React, VueJS, NodeJS",
+		"Nov 2005 to March 2014: into Web development using PHP, MySQL, JQuery, JavaScript, HTML, CSS",
+		"Prior to Nov 2005: Application development using FoxPro, Visual Basic, PowerBuilder",
+		"Can create POCs, presentations, and wireframes"
+	],
+	academic: [
+		"Master in Computer Management",
+		"Bachelor of Commerce"
+	],
+	skills: [
+		"Angular, Svelte, VueJS, React, Stencil",
+		"JavaScript, TypeScript, HTML5, CSS3, SVG",
+		"Bootstrap, Angular-Material, React-UI, Vuetify, Buefy",
+		"Scrum, Sprint, JIRA, GIT, Confluence",
+		"VS Code, IntelliJ, Nodepad++",
+		"Windows, MacOS, Linux (Ubuntu, Mint)",
+		"Python-Django, Cansandra"
+	],
+	exp: [
+		{
+			dates: '16-Jun-2016 to present',
+			jobTitle: 'Project Lead - System',
+			org: 'Mphasis, Bengaluru',
+			technology: 'Angular, React, NodeJS, MySQL, Svelte, Stencil, HTML5, JavaScript, CSS3',
+			items: [
+				'Currently working as Lead frontend developer with Schneider Electric using Svelte. Created web components using StencilJS',
+				'Worked offshore in Bengaluru with a banking conglomerate on projects in Angular and React',
+				'Worked onsite in Glasgow, UK with a banking conglomerate on Angular based projects',
+				'Developed UI for a POC for Digital Readiness Analysis Tool to create a survey questionnaire for various categories using Angular, Material Design, HTML5, CSS3, JavaScript, API integration',
+				'Developed UI for client in Insurance domain using React, Material-UI, HTML5, CSS3, JavaScript, IndexedDB',
+				'Worked offshore at client-side with Schneider Electric on Management Dashboard using Angular 1.x'
+			]
+		},
+		{
+			dates: '15-Aug-2015 to 14-Jan-2016',
+			jobTitle: 'Senior AngularJS Developer',
+			org: 'Pandera Solutions, Bengaluru',
+			technology: 'Angular-1.x, HTML5, JavaScript, CSS3',
+		},
+		{
+			dates: '20-Aug-2014 to 22-Jan-2015',
+			jobTitle: 'Senior Software Developer',
+			org: 'Basil Communications, Bengaluru',
+			technology: 'HTML5, JavaScript, CSS3',
+		},
+		{
+			dates: 'Apr-2014 to Jun-2014',
+			jobTitle: 'Senior AngularJS Developer',
+			org: 'Satsyil, VA, USA (Remote work)',
+			technology: 'Angular-1.x, HTML5, JavaScript, CSS3',
+		},
+		{
+			dates: '23-Nov-2011 – 07-Mar-2014',
+			jobTitle: 'Senior Software Developer (Consultant)',
+			org: 'Tejas Systems / SUNSYSTEMS, Bengaluru :: Client: MModal Global Services',
+			technology: 'PHP, MySQL, jQuery, HTML5, JavaScript, CSS3, Angular-1.x',
+		},
+		{
+			dates: '12-Feb-2010 –22-Sep-2011',
+			jobTitle: 'Senior Software Engineer',
+			org: 'TechJini Solutions, Bengaluru',
+			technology: 'PHP, MySQL, jQuery, HTML5, JavaScript, CSS3, am-charts',
+		},
+		{
+			dates: '25-Jul-2007 – 14-Mar-2009',
+			jobTitle: 'Senior Software Engineer',
+			org: 'TLI Software, Bengaluru',
+			technology: 'PHP, MySQL, jQuery, HTML5, JavaScript, CSS3',
+		},
+	]
+};
