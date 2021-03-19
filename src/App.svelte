8313c7bcb4e58fb3531ead4b793dd7dfd0656406
srikantgudi <srikantgudi@gmail.com>
2021-03-20 04:06:57 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -7,143 +7,103 @@
 </script>
 <style>
 	#app {
-		width: 60vw;
-		height: auto;
+		width: 80vw;
+		height: 94vh;
 		margin: 0 auto;
 		padding: 10px;
-		background: linear-gradient(lightblue,lightcyan,lightblue);
+		background: linear-gradient(#023e8a,#00b4d8,#023e8a);
 		color: navy;
 		line-height: 1.2;
 		font-family: Montserrat;
 	}
-	.info {
-		position: absolute;
-		top: 2em;
+	.page-header {
 		display: flex;
 		flex-flow: column;
-		align-items: center;
-		box-shadow: 0 2px 4px #023e8a;
-		padding: 4px;
-		border-radius: 50%;
-		transition: all 0.5s;
-		background: linear-gradient(#90e0ef,#caf0f8,#90e0ef);
-		color: navy;
-	}
-	.info.active {
-		box-shadow: 0 2px 14px #457b9d;
-		border-radius: 10%;
-	}
-	.infobtn {
-		cursor: pointer;
-		display: flex;
-		align-items: center;
-		justify-content: space-around;
-		width: 1em;
-		border-radius: 50%;
-	}
-	.infobtn.hide {
-		display: none;
-	}
-	.infoclose {
-		cursor: pointer;
-		border: none;
-		border-radius: 50%;
-		background-color: lightgrey;
-	}
-	.info-content {
-		display: none;
-	}
-	.info-content.active {
-		display: flex;
-		flex-flow: column;
-		gap: 10px;
-		transition: all 1s;
-		height: 15vh;
-	}
-	.grad-lite {
-		background: linear-gradient(lightcyan,lightblue);
-		color: navy;
-	}
-	.grad-dark {
-		background: linear-gradient(grey, darkslategrey);
-		color: whitesmoke!important;
-	}
-	.profile {
-		font-family: 'Helvetica Neue';
-	}
-	.pagetitle {
-		height: 3em;
-		background: linear-gradient(#023e8a,#caf0f8,#023e8a);
-		color: whitesmoke;
-		border-radius: 1em;
 		text-align: center;
-		padding: 10px;
+		min-height: 10vh;
+		width: 80vw;
+		margin: 0 auto;
+		border-radius: 20px 20px 0 0;
+		margin-bottom: 10px;
+		box-shadow: 0 4px 10px #999;
+		border-radius: 20px 20px 10px 10px;
+		color: #f5f3f4;
 	}
 	.name {
 		font-family: Righteous;
-		font-weight: 600;
-		font-size: 3vw;
-		letter-spacing: 1px;
-		text-transform: uppercase;
-		color: #1d3557;
-	}
-	@keyframes animPositionTitle {
-		from {color: beige;}
-		to {color: #03045e;}
+		font-size: 24px;
+		letter-spacing: 2px;
 	}
 	.positiontitle {
-		font-size: 2vw;
+		font-size: 20px;
 		font-family: Ubuntu;
 		font-weight: 600;
-		padding: 1vw 1vh;
+		padding: 1vh 2vw;
 		margin: 1vh 4vw;
 		background: linear-gradient(#343a40,#dee2e6,#343a40);
-		color: #6d6875;
+		color: #161a1d;
 		border-radius: 1em;
 		animation: animPositionTitle 5s ease-in-out infinite reverse;
 	}
-	.space-around {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
+	.panel {
+		border-radius: 20%;
+		box-shadow: 0 0 4px #999;
+		height: 15vh;
+		overflow: hidden;
+		width: 50vw;
+		margin: 8px auto;
+		transition: all 0.5s;
 	}
-	.sectiontitle {
-		display: flex;
-		flex-flow: column;
-		margin-top: 10px;
-		font-size: 1.8vw;
+	.panel-header {
 		font-family: Orbitron;
-		letter-spacing: 2px;
-		color: navy;
-		padding: 2vh 2vw;
-		background-color: lightblue;
-		border-radius: 20px 20px 0 0;
-	}
-	.profile {
-		font-family: Ubuntu;
-		line-height: 1.5;
-		padding: 1em;
-		transition: all 1s;
-	}
-	.profile:hover {
-		font-weight: 600;
-	}
-	.contact-item {
-		min-width: 30%;
+		position: relative;
+		top: 6vh;
+		font-size: 20px;
 		text-align: center;
+		transition: all 1s;
+		background-color: #999;
+		border-radius: 10px;
+		color: #f9f9f9;
+		padding: 10px;
 	}
-	.box {
-		box-shadow: 0 2px 20px #999999;
-		border-radius: 0 0 10px 10px
+	.panel-content {
+		opacity: 0.2;
+		font-size: 10px;
+		font-family: Montserrat;
+		color: #f5f3f4;
+	}
+	.panel:hover {
+		border-radius: 10px;
+		width: 70vw;
+		height: 40vh;
+		overflow-y: hidden;
+		box-shadow: 0 4px 10px #009;
+	}
+	.panel:hover > .panel-header {
+		letter-spacing: 3px;
+		font-size: 18px;
+		font-weight: 600;
+		top: 0;
+		border-radius: 10px 10px 0 0;
+		color: beige;
+		background: linear-gradient(lightblue,blue);
+	}
+	.panel:hover > .panel-content {
+		opacity: 1;
+		font-size: 16px;
+	}
+	.panel:hover * .skills {
+		overflow-y: auto;
+	}
+	.panel-inner {
+		height: 30vh;
+		overflow-y: unset;
+	}
+	.panel:hover * .panel-inner {
+		overflow-y: auto;
 	}
 	.list {
 		padding: 10px 20px;
-	}
-	.edu {
-		font-size: 12px;
-	}
-	.edu:nth-child(n+1):before {
-		content: '\bb\20';
 	}
 	.skills {
 		display: grid;
@@ -151,6 +111,9 @@
 		font-family: Roboto;
 		font-size: 14px;
 		padding: 10px;
+		font-family: Montserrat;
+		height: 30vh;
+		overflow-y: hidden;
 	}
 	.skill {
 		transition: all 1s;
@@ -159,7 +122,6 @@
 		text-align: center;
 		padding: 4px;
 		margin: 0 4px 4px 0;
-		font-family: Ubuntu;
 		font-size: 18px;
 		border-radius: 4px;
 	}
@@ -182,31 +144,18 @@
 	}
 	.jobtitle {
 		font-family: Montserrat;
-		font-size: 1.5vw;
+		font-size: 16px;
 		background-color: darkslategrey;
 		padding: 4px 10px;
 		color: whitesmoke;
 		border-radius: 10px 10px 0 0;
 	}
 	@media screen and (max-width: 720px) {
-		.info {
-			top: 5vh;
-			margin-left: 1em;
-			padding: 2px 0 4px;
-			font-size: 12px;
-			height: auto;
+		.panel-content {
+			font-size: 16px;
 		}
-		.pagetitle {
-			height: auto;
-			padding: 10px;
-		}
-		.sectiontitle {
-			font-size: 80%;
-		}
-		.edu-list, .list {
+		.list {
 			width: 100%;
-			font-size: 8px;
-			font-family: sans-serif;
 			padding: 0;
 		}
 		.positiontitle {
@@ -217,97 +166,73 @@
 			grid-template-columns: 1fr 1fr;
 			line-height: 1.1;
 		}
-		.skill, .edu {
-			font-size: 10px;
+		.skill {
+			font-size: 16px;
 		}
 	}
 </style>
-<div id="app" class="grad-app">
-	<div class="topnav grad-lite">
-		<div class="pagetitle">
-			<div class="name">
-				Srikant Gudi
-			</div>
-			<div class="info" class:active={showInfo} on:click={() => {showInfo = true}}>
-				<div class="infobtn" class:hide={showInfo} >&quest;</div>
-				<div class="info-content" class:active={showInfo}>
-					<div class="contact-item">
-						Bengaluru, India 
-						<button class="infoclose" on:click|stopPropagation={() => {showInfo=false}}>&times;</button>
-					</div>
-					<div class="contact-item">
-						srikantgudi@gmail.com
-					</div>
-					<div class="contact-item">
-						+91 829 665 6336
-					</div>
-					<div class="edu-list">
-						<div class="edu">Master in Computer Management</div>
-						<div class="edu">Bachelor of Commerce</div>
-					</div>
-				</div>
-			</div>
+<div id="app">
+	<div class="page-header">
+		<div class="name">Srikant Gudi</div>
+		<div>
+			Bengaluru, India | 
+			srikantgudi@gmail.com |
+			+91 829 665 6336
 		</div>
-		
-		<div class="positiontitle space-around">
-			Senior Front-end Engineer
-		</div>
+		<div class="positiontitle">Senior Front-end Engineer</div>
 	</div>
-	<div>
-		<div class="sectiontitle grad-dark space-around">
-			&laquo; PROFILE &raquo;
+	<div class="panel">
+		<div class="panel-header">
+			Profile
 		</div>
-		<div class="box profile">
-			<ul>
-				{#each data.profile as text}
-				<li>{text}</li>
-				{/each}
-			</ul>
-		</div>
-	</div>
-	<div>
-		<div class="sectiontitle grad-dark space-around">
-			&laquo; TECHNICAL SKILLS &raquo;
-		</div>
-		<div class="box list">
-			<div class="skills">
-				{#each data.skills as text}
-				<div class="skill">{text}</div>
-				{/each}
+		<div class="panel-content">
+			<div class="panel-inner">
+				<ul><li>Senior software professional with over 20 years of experience in various technologies</li><li>Currently into frontend development using Angular, Svelte, Stencil, React, NodeJS From April 2014</li><li>Working knowledge of Python-Django, MongoDB</li><li>Proficient in VueJS, creating web components using StencilJS, creating RESTful APIs based on MySQL, MongoDB</li><li>Proficient in creating POCs, prototypes, presentations, and wireframes</li><li>Can conduct trainings in the known technologies</li><li>Can mentor juniors and freshers</li><li>Interested in frontend architechture, providing solutions as independent developer</li></ul>
 			</div>
 		</div>
 	</div>
-	
-	<div>
-		<div class="sectiontitle grad-dark space-around">
-			&laquo; WORK EXPERIENCE &raquo;
+	<div class="panel">
+		<div class="panel-header">
+			Technical Skills
 		</div>
-		<div class="box list">
-			{#each data.exp as item}
-				<div class="exp box">
-					<div class="jobtitle">
-						{item.jobTitle} :: {item.dates} &raquo;
-					</div>
-					<div class="expdetail">
-						<div>
-							{item.org}
+		<div class="panel-content">
+			<div class="skills"><div class="skill">Angular</div><div class="skill">Svelte</div><div class="skill">VueJS</div><div class="skill">React</div><div class="skill">Stencil</div><div class="skill">JavaScript</div><div class="skill">TypeScript</div><div class="skill">HTML5</div><div class="skill">CSS3</div><div class="skill">SVG</div><div class="skill">Bootstrap</div><div class="skill">Angular-Material</div><div class="skill">React-UI</div><div class="skill">Vuetify</div><div class="skill">Buefy</div><div class="skill">Scrum</div><div class="skill">Sprint</div><div class="skill">JIRA</div><div class="skill">GIT</div><div class="skill">Confluence</div><div class="skill">VS Code</div><div class="skill">IntelliJ</div><div class="skill">Nodepad++</div><div class="skill">Windows</div><div class="skill">MacOS</div><div class="skill">Linux</div><div class="skill">Python-Django</div><div class="skill">Casandra</div></div>
+		</div>
+	</div>
+	<div class="panel">
+		<div class="panel-header">
+			Work History
+		</div>
+		<div class="panel-content">
+			<div class="panel-inner list">
+				{#each data.exp as item}
+					<div class="exp box">
+						<div class="jobtitle">
+							{item.jobTitle} :: {item.dates} &raquo;
 						</div>
-						<div>
-							Technologies used: {item.technology}
-						</div>	
-						<div class="details">
-							<details>
-								<summary>Details</summary>
-								<ul>
-									{#each item.content as text}
-									<li>{text}</li>
-									{/each}
-								</ul>
-							</details>
+						<div class="expdetail">
+							<div>
+								{item.org}
+							</div>
+							<div>
+								Technologies used: {item.technology}
+							</div>	
+							<div class="details">
+								<details>
+									<summary>Details</summary>
+									<ul>
+										{#each item.content as text}
+										<li>{text}</li>
+										{/each}
+									</ul>
+								</details>
+							</div>
 						</div>
 					</div>
-				</div>
-			{/each}
+				{/each}
+			</div>
 		</div>
-	</div>
+	</div>	
 </div>
+
+
