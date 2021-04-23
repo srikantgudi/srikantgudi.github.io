d941f4d9f9bb422eb873529e769191701d3c2bd3
srikantgudi <srikantgudi@gmail.com>
2021-04-23 15:52:25 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Container, Radio, Row, Col, MaterialApp } from 'svelte-materialify';
+	import { Container, Button, Row, Col, MaterialApp } from 'svelte-materialify';
 
 	import Resume1 from './Resume1.svelte';
 	import Resume2 from './Resume2.svelte';
@@ -32,18 +32,26 @@
 	.resume-view {
 		margin-top:4em;
 	}
+	.resume-btn {
+		background-color: whitesmoke;
+		padding: 1em;
+		color: #000;
+	}
+	.resume-btn.active {
+		background: linear-gradient(lightblue, lightcyan, lightblue);
+	}
 </style>
 
 <MaterialApp theme={"light"}>
 	<Container fluid>
 		<div class="topbar white elevation-4">
 			<div>Choose resume to view:</div>
-			<Radio bind:group={resume} class="ml-4" value="r1">
-				<span class="text-h5">Resume with custom styles</span>
-			</Radio>
-			<Radio bind:group={resume} class="ml-4" value="r2">
-				<span class="text-h5">Resume with Svelte-Materialify</span>
-			</Radio>
+			<Button class="text-h5 mr-2" on:click={() => setResume('r1')}>
+				<div class="resume-btn" class:active={resume==='r1'}>Resume with custom styles</div>
+			</Button>
+			<Button class="text-h5" on:click={() => setResume('r2')}>
+				<div class="resume-btn" class:active={resume==='r2'}>Resume with Svelte-Materialify</div>
+			</Button>
 		</div>
 		<div dense class="resume-view">
 			<svelte:component this={resumes[resume]} />
