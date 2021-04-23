58a8175a04cc5688b3ba337c008eb8f3869d8d8a
srikantgudi <srikantgudi@gmail.com>
2021-04-23 15:34:05 +0530

    Adding left-out files

--- src/Topnav.svelte
+++ src/Topnav.svelte
@@ -1,9 +1,13 @@
 <div class="topnav">
-  <div class="title">SRIKANT GUDI</div>
-  <div class="position-title">SENIOR FRONTEND ENGINEER</div>
+  <div class="title">
+    <div class="name">SRIKANT GUDI</div>
+    <div class="position">Senior Front-End Professional</div>
+  </div>
   <div class="contact">
     <div class="contact-item">Bengaluru,  India</div>
     <div class="contact-item">srikantgudi@gmail.com</div>
-    <div class="contact-item">+91 829 665 6336</div>
+    <div class="contact-item">&phone; +91 829 665 6336</div>
+    <div class="contact-item"><a href="https://www.linkedin.com/in/srikantgudi-blr" target="_blank">LinkedIN</a></div>
+    <div class="contact-item">Skype: srikantgudi</div>
   </div>
 </div>
